bee8ce503212c23476629b594a0a5c206c0961b4
MouZer77 <Skymouser@gmail.com>
2023-06-15 19:22:30 -0400

    Pseudodelic

---
+++ pscode-exercise.py
@@ -0,0 +1,39 @@
+# // Exercise 1: Calculate the Sum of Two Numbers
+#
+#
+# // Exercise 2: Check if a Number is Even or Odd
+#
+#
+# // Exercise 3:
+# Write pseudo code that will perform the following.
+#
+# a) Read in 5 separate numbers.
+# b) Calculate the average of the five numbers.
+# c) Find the smallest (minimum) and largest (maximum) of the five entered numbers.
+# d) Write out the results found from steps b and c with a message describing what they are
+#
+#
+#
+# Exercise 1:
+#
+# Request input number 1
+# Request input number 2
+# Var to add input1 and 2.
+# Print var
+#
+# Exercise 2:
+# Request input number for JUDGMENT!
+# Write IF statement
+# If the remainder of number is 0, print Even
+# ELSE print Odd
+# Exercise 3:
+# A.) Var 1 = Request input for for 5 number
+#
+# B.) Var 2 = var 1 added together
+# Var 3 = var 1 as length
+# Var 4 = var 2 divided by var 3 length
+#
+# C.) Var 5 = Var 1 ()min
+# Var 6 = Var 1 ()max
+#
+# D.) print Outputs Var with str
